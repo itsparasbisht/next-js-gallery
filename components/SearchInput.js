86a129ef41bf8d3527356fd4a89eb03f41b65f83
itsparasbisht <iamparasbisht@gmail.com>
2022-07-28 20:42:18 +0530

    get suggestions pai

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import styles from "./searchInput.module.css";
 
+let debounceHandler = null;
+
 function SearchInput() {
+  const [search, setSearch] = useState();
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    clearTimeout(debounceHandler);
+    debounceHandler = setTimeout(() => {
+      getSuggestions(search);
+    }, 700);
+  }, [search]);
+
+  const getSuggestions = (search) => {
+    fetch(`https://api.datamuse.com/sug?s=${search}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const suggestionsArr = data;
+        setSuggestions(suggestionsArr);
+      });
+  };
+
   return (
     <div className={styles.searchContainer}>
       <form>
-        <input type="text" placeholder="search for images..." />
+        <input
+          type="text"
+          placeholder="search for images..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <h3>Images from Unsplash</h3>
       </form>
     </div>
